2b73274a387addbf560b96e6b0a982cc2b761a81
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2025-05-06 17:35:45 +0000

    upado repositorio.

---
+++ roteAutentication/src/main/java/kickOff/roteAutentication/RoteAutenticationApplication.java
@@ -0,0 +1,13 @@
+package kickOff.roteAutentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoteAutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoteAutenticationApplication.class, args);
+	}
+
+}
---
+++ roteAutentication/src/test/java/kickOff/roteAutentication/RoteAutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package kickOff.roteAutentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoteAutenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
